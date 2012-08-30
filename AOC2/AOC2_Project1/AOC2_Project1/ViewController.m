f5ad6a7a699a27682ffb95bb08086251ccdba9ef
roninmoto <roninmoto@gmail.com>
2012-08-30 17:04:57 -0400

    created second set of uilabels

--- AOC2/AOC2_Project1/AOC2_Project1/ViewController.m
+++ AOC2/AOC2_Project1/AOC2_Project1/ViewController.m
@@ -21,7 +21,7 @@
     //Labels for classes
     
     //Dell Label
-    DellLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 5, 320, 70)];
+    DellLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 1, 320, 70)];
     DellLabel.backgroundColor = [UIColor lightGrayColor];
     DellLabel.textColor = [UIColor blackColor];
     DellLabel.textAlignment = UITextAlignmentCenter;
@@ -29,7 +29,7 @@
     [self.view addSubview:DellLabel];
     
     //HP Label
-    HPLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 85, 320, 70)];
+    HPLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 74, 320, 70)];
     HPLabel.backgroundColor = [UIColor lightGrayColor];
     HPLabel.textColor = [UIColor blackColor];
     HPLabel.textAlignment = UITextAlignmentCenter;
@@ -37,12 +37,39 @@
     [self.view addSubview:HPLabel];
     
     //ASUS Label
-    ASUSLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 165, 320, 70)];
+    ASUSLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 147, 320, 70)];
     ASUSLabel.backgroundColor = [UIColor lightGrayColor];
     ASUSLabel.textColor = [UIColor blackColor];
     ASUSLabel.textAlignment = UITextAlignmentCenter;
     ASUSLabel.numberOfLines = 3;
     [self.view addSubview:ASUSLabel];
+    
+    //Adding second set of UILabels for each subclass
+    //dell label two
+    DellLabelTwo = [[UILabel alloc] initWithFrame:CGRectMake(0, 220, 320, 70)];
+    DellLabelTwo.backgroundColor = [UIColor blueColor];
+    DellLabelTwo.textColor = [UIColor whiteColor];
+    DellLabelTwo.textAlignment = UITextAlignmentCenter;
+    DellLabelTwo.numberOfLines = 3;
+    [self.view addSubview:DellLabelTwo];
+    
+    //hp label two
+    HPLabelTwo = [[UILabel alloc] initWithFrame:CGRectMake(0, 293, 320, 70)];
+    HPLabelTwo.backgroundColor = [UIColor blueColor];
+    HPLabelTwo.textColor = [UIColor whiteColor];
+    HPLabelTwo.textAlignment = UITextAlignmentCenter;
+    HPLabelTwo.numberOfLines = 3;
+    [self.view addSubview:HPLabelTwo];
+    
+    //ASUS label two
+    ASUSLabelTwo =[[UILabel alloc] initWithFrame:CGRectMake(0, 367, 320, 70)];
+    ASUSLabelTwo.backgroundColor = [UIColor blueColor];
+    ASUSLabelTwo.textColor = [UIColor whiteColor];
+    ASUSLabelTwo.textAlignment = UITextAlignmentCenter;
+    ASUSLabelTwo.numberOfLines = 3;
+    [self.view addSubview:ASUSLabelTwo];
+    
+    
     
     //First accessor label which connects to subclass dell
     DellComputer *dell = (DellComputer*) [ComputerFactory OEMFactory:0];
@@ -60,6 +87,10 @@
     ASUSComputer *ASUS = (ASUSComputer*) [ComputerFactory OEMFactory:2];
     [ASUS setHDSize:1];
     ASUSLabel.text = [NSString stringWithFormat:@"I am an %@ computer,with an %@ processor running at %iGhz costing around $%i",[ASUS getClassType],[ASUS getCPUType],[ASUS getCPU_Speed],[ASUS getCost]];
+    
+    
+    
+    
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
