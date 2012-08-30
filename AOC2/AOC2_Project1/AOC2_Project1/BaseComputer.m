454fe75b106c60be9edc22509ec8ba05438d6e17
roninmoto <roninmoto@gmail.com>
2012-08-30 12:06:10 -0400

    was getting implementation error and throwing exceptions in code.  Found I had wrongly named one of my varibales.

--- AOC2/AOC2_Project1/AOC2_Project1/BaseComputer.m
+++ AOC2/AOC2_Project1/AOC2_Project1/BaseComputer.m
@@ -9,5 +9,40 @@
 #import "BaseComputer.h"
 
 @implementation BaseComputer
+-(id)init
+{
+    self = [super init];
+    if (self !=nil)
+    {
+        count = 0;
+        classType = @"My computer is";
+        CPUType = @"unknown";
+        CPU_Speed = 0;
+        HD_Size = 0;
+    }
+    return self;
+};
+
+-(void)setHDSize:(int)newHDSize
+{
+    HD_Size = newHDSize;
+}
+
+-(NSString*)getClassType
+{
+    NSLog(@"I am a %@ name brand computer.", classType);
+    return classType;
+}
+
+-(NSString*)getCPUType
+{
+    NSLog(@"I am this type of CPU:%@",CPUType);
+    return CPUType;
+}
+-(int)getCPU_Speed
+{
+    NSLog(@"I run at this speed:%i Ghz",CPU_Speed);
+    return CPU_Speed;
+}
 
 @end
