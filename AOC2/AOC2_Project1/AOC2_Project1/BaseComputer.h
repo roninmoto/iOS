5d732c8a038957473592e7158c083182eb31b1c7
roninmoto <roninmoto@gmail.com>
2012-08-30 12:06:54 -0400

    added my void, int nsstring statements

--- AOC2/AOC2_Project1/AOC2_Project1/BaseComputer.h
+++ AOC2/AOC2_Project1/AOC2_Project1/BaseComputer.h
@@ -7,14 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+typedef enum {
+    OEM_Dell = 0,
+    OEM_HP,
+    OEM_ASUS,
+}myOEM;
 
 @interface BaseComputer : NSObject
 
 {
 @protected
+    myOEM count;
     NSString *classType;
     NSString *CPUType;
     int CPU_Speed;
     int HD_Size;    
 }
+
+
+-(NSString*)getClassType;
+-(NSString*)getCPUType;
+-(int)getCPU_Speed;
+-(void)setHDSize:(int)newHDSize;
+
 @end
