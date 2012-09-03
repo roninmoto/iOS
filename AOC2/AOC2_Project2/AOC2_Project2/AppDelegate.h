4a47f5bfab70c44efe041696681f755871138624
roninmoto <roninmoto@gmail.com>
2012-09-03 11:06:14 -0400

    initial start of project

---
+++ AOC2/AOC2_Project2/AOC2_Project2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC2_Project2
+//
+//  Created by Joel Betterly on 9/3/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AOC2/AOC2_Project2/AOC2_Project2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2_Project2
+//
+//  Created by Joel Betterly on 9/3/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
