83cb09292230098a35e4d1bda39dff9bcb0e818b
roninmoto <roninmoto@gmail.com>
2012-09-04 15:05:47 -0400

    added second view contoller

---
+++ AOC2/AOC2_Project2/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  AOC2_Project2
+//
+//  Created by Joel Betterly on 9/4/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ AOC2/AOC2_Project2/SecondViewController.m
@@ -0,0 +1,44 @@
+//
+//  SecondViewController.m
+//  AOC2_Project2
+//
+//  Created by Joel Betterly on 9/4/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
