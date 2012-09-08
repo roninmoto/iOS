8e02860a6d5f27cc283197eedf90fc1d22ab3e81
roninmoto <roninmoto@gmail.com>
2012-09-08 16:08:40 -0400

    added clear function and got addition to work

--- AOC2/AOC2_Project2/AOC2_Project2/ViewController.m
+++ AOC2/AOC2_Project2/AOC2_Project2/ViewController.m
@@ -22,27 +22,53 @@
 {
     if (onOff.on) {
         label.text = @"Calculator On";
+        displayNumber = 0;
     }
     else
     {
         label.text = @"Calculator Off";
         display.text = @"";
+
     }
 }
 
+// This will display the number in my text field
+//set tags 0-9 for numbers
 
 -(IBAction)buttonNumberPressed:(id)sender
 {
-    if (onOff != false)
+    if (onOff.on != false)
     {
         displayNumber = displayNumber*10 + (float)[sender tag];
         display.text = [NSString stringWithFormat:@"%2.0f",displayNumber];
     }
-    else
-    {
+}
+
+//This will add the previous number to the new number
+//set tag for plus and equal
+
+-(IBAction)clickOperator:(id)sender
+{
+    if (operator == 0) operatorResults = displayNumber;
+        else
+        {
+            operatorResults = operatorResults + displayNumber;
+        }
+    
+    displayNumber = 0;
+    display.text = [NSString stringWithFormat:@"%2.0f", operatorResults];
+    if ([sender tag] == 0) operatorResults = 0;
+    operator = [sender tag];
+}
 
 
-    }
+//clear function to resart at zero
+
+-(IBAction)clearButton:(id)sender
+{
+    displayNumber = 0;
+    display.text = @"0";
+    operator = 0;
 }
 
 //Wendy's video UISegmentControl movie, look around 2:30 in video.  Copied from there.
@@ -69,9 +95,11 @@
     }
 }
 
+
+
 - (void)viewDidLoad
 {
-    //Set Background Color
+    //Set Background Color to initial color of white
    self.view.backgroundColor = [UIColor whiteColor];
 
     
