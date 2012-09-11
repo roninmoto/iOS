03fd4ead05d440e93f985bec73f278c1ce4a33b8
roninmoto <roninmoto@gmail.com>
2012-09-11 11:26:52 -0400

    created second view controller

---
+++ AOC2/ACO2_Project3/ACO2_Project3/PickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PickerViewController.h
+//  ACO2_Project3
+//
+//  Created by Joel Betterly on 9/11/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerViewController : UIViewController
+
+@end
