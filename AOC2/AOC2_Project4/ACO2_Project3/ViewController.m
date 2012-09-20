e66a288f50a3e170aa90bd629cd3cc98830c70d1
roninmoto <roninmoto@gmail.com>
2012-09-20 14:04:18 -0400

    wrote the save and erase function

--- AOC2/AOC2_Project4/ACO2_Project3/ViewController.m
+++ AOC2/AOC2_Project4/ACO2_Project3/ViewController.m
@@ -14,6 +14,43 @@
 
 @implementation ViewController
 
+
+//Action to save events to memory location on drive
+-(IBAction)saveEventToMemory:(id)sender
+{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (defaults !=nil)
+    {
+        NSString *currentEventString = currentEvents.text;
+        [defaults setObject:currentEventString forKey:@"events"];
+        [defaults synchronize];
+        
+        //telling user that data has been saved and how it will show the next time.
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert"
+                                                      message:@"Data saved to memory.  Data will autoload when you open the app again."
+                                                      delegate:self
+                                                      cancelButtonTitle:@"Okay"
+                                                      otherButtonTitles:nil];
+        [alertView show];
+    }
+}
+//added button to erase data since testing showed a large build up of data
+-(IBAction)erase:(id)sender
+{
+    [[NSUserDefaults standardUserDefaults] setPersistentDomain:[NSDictionary dictionary] forName:[[NSBundle mainBundle]bundleIdentifier]];
+    currentEvents.text = @"";
+    
+    //adding an alert so user knows data has been removed. Could have made an if/else statement but ran out of time to ask user are they sure.
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert"
+                                                        message:@"Your data has been erased."
+                                                       delegate:self
+                                              cancelButtonTitle:@"Okay"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
+// added swipe gesture for swiping to right on main page
 -(void) goSwipe:(UISwipeGestureRecognizer*)recognizer
 {
     if (recognizer.direction == UISwipeGestureRecognizerDirectionRight)
@@ -27,7 +64,7 @@
     }
   }
 }
-
+//part of swiping action
 -(void)viewWillAppear:(BOOL)animated
 {
     swipeToTheRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(goSwipe:)];
@@ -43,6 +80,14 @@
 
 - (void)viewDidLoad
 {
+    //brings up saved event and loads it into textview
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (defaults !=nil)
+    {
+        NSString *currentEventString = [defaults objectForKey:@"events"];
+        currentEvents.text = currentEventString;
+    }
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
