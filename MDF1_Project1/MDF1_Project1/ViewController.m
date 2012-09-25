0f67e74280a73ad5597c6e1ef7b08aeb48f15d5e
roninmoto <roninmoto@gmail.com>
2012-09-25 11:32:15 -0400

    intial start of project.  Added icons and display.

---
+++ MDF1_Project1/MDF1_Project1/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  MDF1_Project1
+//
+//  Created by Joel Betterly on 9/25/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+//image used for icon found at www.iconfinder.com/34303/256/ GPL License Designer Everaldo Coelho
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MDF1_Project1/MDF1_Project1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MDF1_Project1
+//
+//  Created by Joel Betterly on 9/25/12.
+//  Copyright (c) 2012 Joel Betterly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
